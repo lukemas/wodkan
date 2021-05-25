b67c3bdf33d9ba2ec778da758dbcb6f51d68e424
Łukasz Masłowski <l.maslowski91@gmail.com>
2021-05-25 07:29:02 +0200

    join-us  w/out media-queries & emergency v. alpha

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -23,7 +23,7 @@ get_header();
 
         <div class="hero-card d-block d-sm-inline">
             <div class="hero-card-logo">
-            <img src="http://wk.local/wp-content/uploads/2021/05/file1.png">
+            <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/file-icon.png">
             </div>
             <div class="hero-card-heading">
                 BOK
@@ -31,7 +31,7 @@ get_header();
         </div>
         <div class="hero-card d-block">
             <div class="hero-card-logo">
-            <img src="http://wk.local/wp-content/uploads/2021/05/file1.png">
+            <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/file-icon.png">
             </div>
             <div class="hero-card-heading">
                 Podpisz umowę
@@ -39,7 +39,7 @@ get_header();
         </div>
         <div class="hero-card d-block">
             <div class="hero-card-logo">
-                <img src="http://wk.local/wp-content/uploads/2021/05/file1.png">
+                <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/file-icon.png">
             </div>
             <div class="hero-card-heading">
                 Zgłoś stan licznika
@@ -47,7 +47,7 @@ get_header();
         </div>
         <div class="hero-card d-block sm-d-inline">
             <div class="hero-card-logo">
-            <img src="http://wk.local/wp-content/uploads/2021/05/file1.png">
+            <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/file-icon.png">
             </div>
             <div class="hero-card-heading">
                 Zgłoś awarię
@@ -55,7 +55,7 @@ get_header();
         </div>
         <div class="hero-card d-block">
             <div class="hero-card-logo">
-            <img src="http://wk.local/wp-content/uploads/2021/05/file1.png">
+            <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/file-icon.png">
             </div>
             <div class="hero-card-heading">
                 Jakość wody
@@ -63,8 +63,169 @@ get_header();
         </div>
         </div>
         </div>
+        </div></section>
+
+<section class="news">
+    <div class="container">
+        <div class="news-container">
+    <div class="headings-news-container col-lg-6 col-md-12">
+        <h4>// Najnowsze informacje dla klientów</h4>
+        <h2>Aktualności i Komunikaty</h2>
+    </div>
+    <div class="read-all-button col-6">
+        <a class="read-all-button-link" href="#">Przeczytaj wszystkie</a>
+    </div>
         </div>
+        <div id="news-box-container" class="container">
+  <div class="row news-box-row">
+    <div class="col-sm-12 news-box">
+        <img class="news-box-image" src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/scientist-examiner-taking-samples-of-water-from-an-underground-city-sewage-system-to-analyze-level-of-pollution.png">
+        <div class="category-button"><span>Badanie ścieków</span></div>
+        <div class="news-excerpt-container">
+        <h5 class="news-title">Nowe badania jakości wody</h5>
+        <p class="news-excerpt-text">id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla</p>
+<div class="date-and-read-more-button-container row">
+    <div class="date col-4"><span>22.05.2021</span></div>
+    <div class="read-more-button col-8"><a href="#">Przeczytaj<img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/read-more-button-icon.png"></a></div>
+   <a href="#"></a>
+</div>
+    </div>
+    </div>
+    <div class="col-sm-12 news-box">
+        <img class="news-box-image" src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/scientist-examiner-taking-samples-of-water-from-an-underground-city-sewage-system-to-analyze-level-of-pollution.png">
+        <div class="category-button"><span>Badanie ścieków</span></div>
+        <div class="news-excerpt-container">
+        <h5 class="news-title">Nowe badania jakości wody</h5>
+        <p class="news-excerpt-text">id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla</p>
+<div class="date-and-read-more-button-container row">
+    <div class="date col-4"><span>22.05.2021</span></div>
+    <div class="read-more-button col-8"><a href="#">Przeczytaj<img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/read-more-button-icon.png"></a></div>
+   <a href="#"></a>
+</div>
+    </div>
+    </div>
+    <div class="col-sm-12 news-box">
+        <img class="news-box-image" src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/scientist-examiner-taking-samples-of-water-from-an-underground-city-sewage-system-to-analyze-level-of-pollution.png">
+        <div class="category-button"><span>Badanie ścieków</span></div>
+        <div class="news-excerpt-container">
+        <h5 class="news-title">Nowe badania jakości wody</h5>
+        <p class="news-excerpt-text">id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla</p>
+<div class="date-and-read-more-button-container row">
+    <div class="date col-4"><span>22.05.2021</span></div>
+    <div class="read-more-button col-8"><a href="#">Przeczytaj<img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/read-more-button-icon.png"></a></div>
+   <a href="#"></a>
+</div>
+    </div>
+    </div>
+  </div>
+</div>
+</div>
 </section>
+<section class="services services-bg">
+<div class="container">
+<div class="row services-row">
+<div class="services-container services-1 col">
+    <div class="headings-services-container">
+        <h4>// Nasze usługi dla Ciebie</h4>
+        <h2>Dobrze wykonana praca. Zadowolenie na wiele lat.</h2>
+    </div>
+     
+        <div class="white-text text-long">
+            <p>Właściciel nieruchomości (osoba posiadająca tytuł prawny do korzystania z nieruchomości lub osoba korzystająca z nieruchomości o nieuregulowanym stanie prawnym) składa w Biurze Obsługi Klienta „Wniosek o warunki techniczne”</p>
+</div>
+<div class="numbers-container">
+    <div class="clients-number-container  white-text">
+        <div class="number">450+</div>
+        <p>klientów</p>
+</div>
+<div class="clients-number-container  white-text">
+        <div class="number">250+</div>
+        <p>projektów</p>
+</div>
+<div class="clients-number-container  white-text">
+        <div class="number">30+</div>
+        <p>specjalistów</p>
+</div>
+</div>
+</div>
+<div class="services-container services-2 col">
+   <div class="row">
+       <div class="services-box box-1">
+           <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/1.png">
+           <div class="services-box-stripe-row-1">
+               <h3>Dokumentacja projektowa</h3>
+</div>
+        </div>
+        <div class="services-box box-2">
+           <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/2.png">
+           <div class="services-box-stripe-row-1">
+           <h3>Wymiana wodomierza</h3>
+</div>
+        </div>
+       <div class="services-box box-3">
+           <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/3.png">
+           <div class="services-box-stripe-row-2">
+           <h3>Roboty ziemne</h3>
+</div>
+        </div>
+        <div class="services-box box-4">
+           <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/4.png">
+           <div class="services-box-stripe-row-2">
+           <h3>Inspekcja kamerą TV</h3>
+</div>
+        </div>
+</div>
+</div>
+
+</div>
+</div>
+</section>
+
+<section class="services join-us-bg">
+<div class="container">
+<div class="row">
+
+<div class="services-container services-2">
+        <div class="join-us-left-image">
+           <img src="http://lukemastesty.hekko24.pl/wp-content/uploads/2021/05/playing-games-with-my-family.jpg">
+        </div>
+</div>
+<div class="services-container services-1 col">
+    <div class="join-us-headings-container">
+        <h4>// Zostań naszym klientem</h4>
+        <h2>Przyłącz się do Sieci wodociągowej i kanalizacyjnej</h2>
+    </div>
+     
+        <div class="text-long">
+            <p>Właściciel nieruchomości (osoba posiadająca tytuł prawny do korzystania z nieruchomości lub osoba korzystająca z nieruchomości o nieuregulowanym stanie prawnym) składa w Biurze Obsługi Klienta „Wniosek o warunki techniczne”</p>
+</div>
+
+    <div class="download-button">
+   <a class="download-button-link" href="#">pobierz dokument</a>
+</div>
+
+
+
+</div>
+</div>
+</div>
+</section>
+<section class="emergency emergency-bg">
+<div class="container text-center">
+<h5>Potrzebujesz pomocy? chcesz zgłosić awarię?</h5>
+<h2>Pogotowie wodno-kanalizacyjne 24/7</h2>
+<div class="row">
+<div class="download-button">
+   <a class="download-button-link" href="tel:0048427151296">42 715 12 96</a>
+   <span>stacjonarny</span>
+</div>
+<div class="download-button">
+   <a class="download-button-link" href="tel:0048887151296">887 151 296</a>
+   <span>całodobowy</span>
+</div>
+</div>
+</section>
+
 
 <?php
 get_footer();
